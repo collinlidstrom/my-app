f3e0434dbf7708a250ff2b5b0fe85b1c0af5857a
Collin Lidstrom <33526343+collinlidstrom@users.noreply.github.com>
2022-08-19 17:44:43 -0500

    add extract button

--- src/components/ExportComponent.js
+++ src/components/ExportComponent.js
@@ -5,38 +5,52 @@ export default function ExportComponent(props) {
 
     const titleStyle = {
         'paddingLeft': '20px',
-        'border' : '1px',
-        'borderColor' : 'white'
+        'border': '10px',
+        'borderColor': 'white'
     }
 
     return (
         <div>
             <br></br>
-            <div align='center' style={titleStyle}>
+            <div style={titleStyle}>
                 <h4 align='left'>
                     PDF Extract Demo
                 </h4>
                 <br></br>
-                <div className="mb-6">
-                    <label for="formFile" className="form-label">Default file input example</label>
-                    <input className="form-control" type="file" id="formFile" />
-                </div>
-                <div className="mb-3">
-                    <label for="formFileMultiple" className="form-label">Multiple files input example</label>
-                    <input className="form-control" type="file" id="formFileMultiple" multiple />
-                </div>
-                <div className="mb-3">
-                    <label for="formFileDisabled" className="form-label">Disabled file input example</label>
-                    <input className="form-control" type="file" id="formFileDisabled" disabled />
-                </div>
-                <div className="mb-3">
-                    <label for="formFileSm" className="form-label">Small file input example</label>
-                    <input className="form-control form-control-sm" id="formFileSm" type="file" />
-                </div>
-                <div>
-                    <label for="formFileLg" className="form-label">Large file input example</label>
-                    <input className="form-control form-control-lg" id="formFileLg" type="file" />
-                </div>
+                <table>
+                    <tr>
+                        <div className="mb-3">
+                            <label for="formFile" className="form-label">Default file input example</label>
+                            <input className="form-control" type="file" id="formFile" />
+                            <br></br>
+                            <button type="button" className="btn btn-primary">Extract</button>
+                        </div>
+                    </tr>
+                    <tr>
+                        <div className="mb-3">
+                            <label for="formFileMultiple" className="form-label">Multiple files input example</label>
+                            <input className="form-control" type="file" id="formFileMultiple" multiple />
+                        </div>
+                    </tr>
+                    <tr>
+                        <div className="mb-3">
+                            <label for="formFileDisabled" className="form-label">Disabled file input example</label>
+                            <input className="form-control" type="file" id="formFileDisabled" disabled />
+                        </div>
+                    </tr>
+                    <tr>
+                        <div className="mb-3">
+                            <label for="formFileSm" className="form-label">Small file input example</label>
+                            <input className="form-control form-control-sm" id="formFileSm" type="file" />
+                        </div>
+                    </tr>
+                    <tr>
+                        <div>
+                            <label for="formFileLg" className="form-label">Large file input example</label>
+                            <input className="form-control form-control-lg" id="formFileLg" type="file" />
+                        </div>
+                    </tr>
+                </table>
                 <br></br>
                 <br></br>
                 <br></br>
