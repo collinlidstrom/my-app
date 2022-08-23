87a8f1fc9624b3e5ef45f17ff521913965728cb8
Collin Lidstrom <33526343+collinlidstrom@users.noreply.github.com>
2022-08-23 17:33:56 -0500

    added login page

--- src/App.js
+++ src/App.js
@@ -25,11 +25,11 @@ function App() {
   //   paddingRight: '40px'
   // }
 
-  return (<CustomProvider theme={"dark"}>
+  return (<CustomProvider theme={"light"}>
     <div className="App">
       <div className="collapse" id="navbarToggleExternalContent">
         <div className="bg-dark p-4">
-          <h5 className="text-white h4">Sail Demo</h5>
+          {/* <h5 className="text-white h4">Sail Demo</h5> */}
           <span className="text-muted">Providing small businesses with optimization solutions. We offer products and consulting for efficiency, time management and software solutions.</span>
         </div>
       </div>
@@ -40,8 +40,65 @@ function App() {
           </button>
         </div>
       </nav>
-      <Title />
-      <ExportComponent />
+      <div className='container'>
+        <br />
+          <h1 >
+            Sail
+          </h1>
+        <br />
+        <br />
+        <div className="col d-flex justify-content-center">
+          <form>
+            <div className="form-outline mb-4">
+              <input type="email" id="form2Example1" className="form-control" />
+              <label className="form-label" for="form2Example1">Email address</label>
+            </div>
+            <div className="form-outline mb-4">
+              <input type="password" id="form2Example2" className="form-control" />
+              <label className="form-label" for="form2Example2">Password</label>
+            </div>
+
+            <div className="row mb-4">
+              <div className="col d-flex justify-content-center">
+                <div className="form-check">
+                  <input className="form-check-input" type="checkbox" value="" id="form2Example31" />
+                  <label className="form-check-label" for="form2Example31"> Remember me </label>
+                </div>
+              </div>
+
+              <div className="col">
+                <a href="#!">Forgot password?</a>
+              </div>
+            </div>
+
+            <button type="button" className="btn btn-primary btn-block mb-4">Sign in</button>
+
+            <div className="text-center">
+              <p>Not a member? <a href="#!">Register</a></p>
+              <p>or sign up with:</p>
+              <button type="button" className="btn btn-link btn-floating mx-1">
+                <i className="fab fa-facebook-f"></i>
+              </button>
+
+              <button type="button" className="btn btn-link btn-floating mx-1">
+                <i className="fab fa-google"></i>
+              </button>
+
+              <button type="button" className="btn btn-link btn-floating mx-1">
+                <i className="fab fa-twitter"></i>
+              </button>
+
+              <button type="button" className="btn btn-link btn-floating mx-1">
+                <i className="fab fa-github"></i>
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div hidden>
+        <Title />
+        <ExportComponent />
+      </div>
       <Footer>
         <div hidden>
           <List align='center'>
