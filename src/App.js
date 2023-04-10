f5e5aa9cd92c21447be1f011ffe10e067fa60a93
Collin Lidstrom <33526343+collinlidstrom@users.noreply.github.com>
2023-04-10 07:17:06 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,136 +1,57 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Title from "./components/Title";
-import ExportComponent from './components/ExportComponent';
-import "bootstrap"
-import {
-  CustomProvider,
-  Footer,
-  List,
-} from "rsuite";
-import "rsuite/dist/rsuite.min.css";
-import "./App.css";
-import ListItem from "rsuite/esm/List/ListItem";
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [response, setResponse] = useState(null);
 
+  const apiKey = 'your-api-key'; // Replace with your GPT-4 API key
+  const apiUrl = 'https://api.example.com/v1/your-gpt-4-endpoint'; // Replace with the GPT-4 API endpoint
 
-  document.body.style = 'background: ##17A589';
-  // Or with CSS
-  // document.body.classList.add('background-red');
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
 
-  // const spinnerStyle = {
-  //   paddingLeft: '40px',
-  //   paddingTop: '40px',
-  //   paddingRight: '40px'
-  // }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const result = await axios.post(
+        apiUrl,
+        {
+          // Add your request payload here (prompt, max tokens, etc.)
+          prompt: inputText,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiKey}`,
+          },
+        },
+      );
+      setResponse(result.data);
+    } catch (error) {
+      console.error('Error making API call:', error);
+    }
+  };
 
-  return (<CustomProvider theme={"light"}>
+  return (
     <div className="App">
-      <div className="collapse" id="navbarToggleExternalContent">
-        <div className="bg-dark p-4">
-          {/* <h5 className="text-white h4">Sail Demo</h5> */}
-          <span className="text-muted">Providing small businesses with optimization solutions. We offer products and consulting for efficiency, time management and software solutions.</span>
+      <h1>GPT-4 API Example</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Input Text:
+          <input type="text" value={inputText} onChange={handleInputChange} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      {response && (
+        <div>
+          <h2>Response</h2>
+          <pre>{JSON.stringify(response, null, 2)}</pre>
         </div>
-      </div>
-      <nav className="navbar navbar-dark bg-dark">
-        <div className="container-fluid">
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-        </div>
-      </nav>
-      <div className='container'>
-        <br />
-        <h1 >
-          Sail
-        </h1>
-        <br />
-        <br />
-        <div className="col d-flex justify-content-center">
-          <form>
-            <div className="form-outline mb-4">
-              <input type="email" id="form2Example1" className="form-control" />
-              <label className="form-label" for="form2Example1">Email address</label>
-            </div>
-            <div className="form-outline mb-4">
-              <input type="password" id="form2Example2" className="form-control" />
-              <label className="form-label" for="form2Example2">Password</label>
-            </div>
-
-            <div className="row mb-4">
-              <div className="col d-flex justify-content-center">
-                <div className="form-check">
-                  <input className="form-check-input" type="checkbox" value="" id="form2Example31" />
-                  <label className="form-check-label" for="form2Example31"> Remember me </label>
-                </div>
-              </div>
-
-              <div className="col">
-                <a href="#!">Forgot password?</a>
-              </div>
-            </div>
-
-            <button type="button" className="btn btn-primary btn-block mb-4">Sign in</button>
-
-            <div className="text-center">
-              <p>Not a member? <a href="#!">Register</a></p>
-              <p>or sign up with:</p>
-              <button type="button" className="btn btn-link btn-floating mx-1">
-                <i className="fab fa-facebook-f"></i>
-              </button>
-
-              <button type="button" className="btn btn-link btn-floating mx-1">
-                <i className="fab fa-google"></i>
-              </button>
-
-              <button type="button" className="btn btn-link btn-floating mx-1">
-                <i className="fab fa-twitter"></i>
-              </button>
-
-              <button type="button" className="btn btn-link btn-floating mx-1">
-                <i className="fab fa-github"></i>
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-      <div hidden>
-        <Title />
-        <ExportComponent />
-      </div>
-      <Footer>
-        <div hidden>
-          <List align='center'>
-            <ListItem>
-              Inventory Management (auto-order)
-            </ListItem>
-            <ListItem>
-              Document Scanning
-            </ListItem>
-            <ListItem>
-              Document Management
-            </ListItem>
-            <ListItem>
-              Email Scanning
-            </ListItem>
-            <ListItem>
-              Time Optimization
-            </ListItem>
-            <ListItem>
-              Record Management
-            </ListItem>
-            <ListItem>
-              Financial Forecasts
-            </ListItem>
-          </List>
-        </div>
-      </Footer>
+      )}
     </div>
-  </CustomProvider>);
+  );
 }
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
 
 export default App;
